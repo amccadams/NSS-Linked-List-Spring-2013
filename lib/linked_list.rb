fadfcfb8d4738b7bf1c986f10885eb69633a0766
amccadams <amccadams@gmail.com>
2013-04-22 14:31:27 -0500

    peer reviewed

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,19 +7,58 @@ class LinkedList
   end
 
   def add_item(payload)
+    next_item = LinkedListItem.new(payload)
+    if @first_item.nil?
+      @first_item = next_item
+    else
+      final_item = @first_item
+      until final_item.last?
+        final_item = final_item.next_list_item
+      end
+      final_item.next_list_item = next_item
+    end        
   end
 
   def get(n)
+    item = @first_item  
+    n.times do 
+      raise IndexError, "Index Error" if item.nil?
+     item = item.next_list_item  
   end
+   item.payload  
+  end 
 
   def last
+    item = @first_item
+    return nil if item.nil?
+    until item.next_list_item.nil?
+      item = item.next_list_item
   end
+  item.payload
+end
 
   def size
+    item = @first_item
+    size=0
+    until item.nil?
+    item = item.next_list_item
+    size += 1
+    end 
+    size
   end
 
   def to_s
+    current_item = @first_item 
+    payloads = ""
+    while current_item 
+      payloads += " "
+      payloads += current_item.payload
+      payloads += ","  unless current_item.last?
+      current_item = current_item.next_list_item
+      end
+    "|#{payloads} |"
   end
+ 
 
   # ========= Bonus ========== #
 
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -2,12 +2,20 @@ class LinkedListItem
   attr_reader :next_list_item
   attr_reader :payload
 
-  def initialize( payload )
+  def initialize(something)
+    @payload = something
   end
 
   def next_list_item= (linked_list_item)
+  if linked_list_item==self
+   raise ArgumentError
+  else
+   @next_list_item= linked_list_item
+  end
   end
 
-  def last?
+  def last? 
+@next_list_item.nil?
+ 
   end
 end
